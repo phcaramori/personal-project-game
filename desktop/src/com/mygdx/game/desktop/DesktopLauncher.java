b9324e6206eda0acd002613518830ac82cec91ae
Pedro Caramori <phcaramori@gmail.com>
2022-02-22 09:46:32 -0300

    created window settings

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Game"; //Window title, change later on
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new Game(), config);
 	}
 }
